02d66a1e6f51176b86aec17cc4e0a0cfc6bb32dd
iam <brian.kibet2@student.moringaschool.com>
2023-06-16 08:21:50 +0000

    commited assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+countDown(10);
+function writeCards(names = "Guadalupe,Ollie,Aki") {
+  const thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+  }
+  return thankYouMessages;
+}
